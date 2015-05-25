5ea892b1d1bb9d32d5dc14d6c30c5219e8dcfe90
MrBoolean <marcandrebinder@gmail.com>
2015-05-25 19:33:31 +0200

    Add editorconfig, remove .jshint rules, 4 to 2 spaces intend, update package dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,31 @@
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-        codeclimate: {
-            coverage: {
-                options: {
-                    file: __dirname + '/../../segony-build/dist/lcov.info',
-                    token: 'ec3ee6b4447b58b59a914d887acfef6706a0a072b36188b254fa54f48d24b9a5 '
-                }
-            }
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    codeclimate: {
+      coverage: {
+        options: {
+          file: __dirname + '/../../segony-build/dist/lcov.info',
+          token: 'ec3ee6b4447b58b59a914d887acfef6706a0a072b36188b254fa54f48d24b9a5 '
         }
-    });
+      }
+    }
+  });
 
-    grunt.loadTasks('tasks');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('coverage', ['codeclimate:coverage']);
-    grunt.registerTask('test', ['test']);
-    grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('coverage', ['codeclimate:coverage']);
+  grunt.registerTask('test', ['test']);
+  grunt.registerTask('default', ['jshint', 'test']);
 };
--- tasks/codeclimate.js
+++ tasks/codeclimate.js
@@ -1,35 +1,36 @@
-var runner = require('child_process').exec,
-    fs     = require('fs'),
-    binary = __dirname + '/../node_modules/.bin/codeclimate';
+var spawnProcess = require('child_process').exec,
+    fs           = require('fs'),
+    path         = require('path'),
+    codeCliMate  = path.join(__dirname, '..', 'node_modules', '.bin', 'codeclimate');
 
 module.exports = function (grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.registerMultiTask('codeclimate', 'Send your coverage to codeclimate.', function () {
-        var done    = this.async(),
-            options = this.options({
-                token: false,
-                file: ''
-            });
-
-        fs.exists(options.file, function (isPresent) {
-            if (false === isPresent) {
-                throw new Error('Cannot find coverage report file "' + options.file + '"');
-            }
-
-            var command = 'CODECLIMATE_REPO_TOKEN=' + options.token + ' ' + binary + ' < ' + options.file;
-
-            runner(command, function (error, result) {
-                if (error) {
-                    throw new Error(
-                        'Something went wrong during the execution of codeclimate. ' +
-                        'Make sure your configuration is valid'
-                    );
-                }
-
-                grunt.log.ok(result);
-                done();
-            });
+  grunt.registerMultiTask('codeclimate', 'Send your coverage to codeclimate.', function () {
+    var done    = this.async(),
+        options = this.options({
+          token: false,
+          file: ''
         });
+
+    fs.exists(options.file, function (isPresent) {
+      if (false === isPresent) {
+        throw new Error('Cannot find coverage report file "' + options.file + '"');
+      }
+
+      var command = 'CODECLIMATE_REPO_TOKEN=' + options.token + ' ' + codeCliMate + ' < ' + options.file;
+
+      spawnProcess(command, function (error, result) {
+        if (error) {
+          throw new Error(
+            'Something went wrong during the execution of codeclimate. ' +
+            'Make sure your configuration is valid'
+          );
+        }
+
+        grunt.log.ok(result);
+        done();
+      });
     });
+  });
 };
