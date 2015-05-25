f4e617fb220938c421bb5f3d8e982c0d7bb2a218
MrBoolean <marcandrebinder@gmail.com>
2015-05-25 19:37:23 +0200

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
   'use strict';
 
-  // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
@@ -12,20 +11,15 @@ module.exports = function (grunt) {
         jshintrc: '.jshintrc'
       }
     },
-    codeclimate: {
-      coverage: {
-        options: {
-          file: __dirname + '/../../segony-build/dist/lcov.info',
-          token: 'ec3ee6b4447b58b59a914d887acfef6706a0a072b36188b254fa54f48d24b9a5 '
-        }
-      }
+    eslint: {
+      target: ['tasks/**/*.js', 'Gruntfile.js']
     }
   });
 
   grunt.loadTasks('tasks');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-eslint');
 
   grunt.registerTask('coverage', ['codeclimate:coverage']);
   grunt.registerTask('test', ['test']);
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['eslint', 'test']);
 };
