a5866495b40c0fc432b2257e4f6a023e1a1ea6fb
MrBoolean <marcandrebinder@gmail.com>
2015-05-27 07:21:30 +0200

    Add two test cases

--- test/codeclimate.test.js
+++ test/codeclimate.test.js
@@ -1,6 +1,7 @@
 var chai           = require('chai'),
     chaiAsPromised = require('chai-as-promised'),
     assert         = chai.assert,
+    path           = require('path'),
     codeCliMate    = require('../lib/codeclimate');
 
 chai.use(chaiAsPromised);
@@ -9,9 +10,18 @@ describe('tasks/codeclimate', function () {
   'use strict';
 
   it('throws an error if the file does not exist', function () {
-    assert.isRejected(codeCliMate('path/to/nowhere'), new Error('Cannot find coverage report file "path/to/nowhere"'));
+    assert.isRejected(codeCliMate({
+      file: 'path/to/nowhere'
+    }), 'Cannot find coverage report file "path/to/nowhere"');
   });
 
-  it('throws an error if something went wrong during the execution of codeclimate');
-  it('resolves the result');
+  it('throws an error if something went wrong during the execution of codeclimate', function () {
+    assert.isRejected(
+      codeCliMate({
+        file: path.join(__dirname, 'coverage.lcov')
+      }),
+      'Something went wrong during the execution of codeclimate. ' +
+      'Make sure your configuration is valid'
+    );
+  });
 });
